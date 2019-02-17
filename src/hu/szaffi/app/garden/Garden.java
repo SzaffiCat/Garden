34c6bcc52f0dae079d5181e54adfd6584f50ba98
SzaffiCat <nakuaat@gmail.com>
2019-02-17 21:25:39 +0100

    dead plants removed from list

--- src/hu/szaffi/app/garden/Garden.java
+++ src/hu/szaffi/app/garden/Garden.java
@@ -8,21 +8,23 @@ import hu.szaffi.app.garden.plant.Plant;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Garden {
 
-    private int plantNum = 3;
     private List<Plant> plants = new ArrayList<>();
+    private List<Plant> livingPlants;
 
     public void simulateOneDay() {
         Material material = Material.getRandomMaterial();
         System.out.println();
         System.out.println(material);
         for (Plant plant :
-                plants) {
+                livingPlants) {
             plant.modifySize(material);
             plant.printData();
         }
+        updateLivingPlants();
     }
 
     public void initPlants() {
@@ -30,10 +32,22 @@ public class Garden {
         plants.add(new Bush(4, "B205"));
         plants.add(new Cactus(2, "C102"));
         plants.add(new Cactus(1, "C150"));
+        this.livingPlants = new ArrayList<>(plants);
     }
 
     public List<Plant> getPlants() {
         List<Plant> plantList = Collections.unmodifiableList(this.plants);
         return plantList;
     }
+
+    private void updateLivingPlants() {
+        for (Plant plant :
+                livingPlants.stream().collect(Collectors.toList())) {
+            if (!plant.isAlive()) {
+                livingPlants.remove(plant);
+            }
+        }
+    }
+
+
 }
